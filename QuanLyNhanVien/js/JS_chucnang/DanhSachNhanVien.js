715790b35d7e21a7da80c418f8b984c578b42c51
ToanPhung <phungmanhtoancd2001@gmail.com>
2021-09-08 22:31:12 +0700

    upload 2nd

--- QuanLyNhanVien/js/JS_chucnang/DanhSachNhanVien.js
+++ QuanLyNhanVien/js/JS_chucnang/DanhSachNhanVien.js
@@ -9,9 +9,35 @@ function DanhSachNhanVien(){
         this.mangNV.push(nv);
     }
 
+    this.timViTri = function(tk){
+        var viTri = -1;
+        this.mangNV.map(function(item,index){
+        //item này chính là 1 nv trong mảng
+        //index: vị trí của ptu trong mảng
+            if (item.taiKhoan == tk){
+                viTri = index;
+            }
+        });
+        return viTri;
+    }
 
+    this.xoaNhanVien = function(tk){
+        var viTri = this.timViTri(tk);
+        if (viTri >= 0){
+            this.mangNV.splice(viTri,1);
+        } else {
+            console.log("Không tìm được");
+        }
+    }
 
-
+    this.capNhatNhanVien = function(nv){
+        var viTri = this.timViTri(nv.taiKhoan);
+        if (viTri >= 0){
+            this.mangNV[viTri] = nv;
+        } else {
+            console.log("Không tìm được");
+        }
+    }
 
 
 
--- QuanLyNhanVien/js/JS_chucnang/main.js
+++ QuanLyNhanVien/js/JS_chucnang/main.js
@@ -4,7 +4,7 @@
 
 //biến toàn cục
 var dsnv = new DanhSachNhanVien();
-
+var nv = new NhanVien();
 
 
 
@@ -46,9 +46,12 @@ function hienThiTable(mang){
         <td>${item.chucVu}</td>
         <td>${item.tongLuong}</td>
         <td>${item.loaiNV}</td>
-
-
-
+        <td>
+        <button class="btn btn-danger" onclick="xoaNV('${item.taiKhoan}')" >Xóa</button>
+        </td>
+        <td>
+        <button id="btnXem" type="button" class="btn btn-primary" onclick="xemChiTiet('${item.taiKhoan}')" data-toggle="modal" data-target="#myModal">Xem</button>
+        </td>
 
         </tr>`;
     });
@@ -69,7 +72,7 @@ function themNV(){
 
     //validation
 
-    //B2: lưu thông tin vào lớp sinh viên
+    //B2: lưu thông tin vào lớp nhân viên
     var nv = new NhanVien(tk,hoTen,email,pass,ngaylam,luong,chucVu,gioLam);
     nv.tongLuong = nv.tinhTongLuong();
     nv.loaiNV = nv.xepLoaiNV();
@@ -77,7 +80,7 @@ function themNV(){
     console.table(nv);
 
     
-    //B3: Lưu sv vào danh sách sinh viên
+    //B3: Lưu sv vào danh sách nhân viên
     dsnv.themNhanVien(nv);
     console.log('mangnv', dsnv.mangNV);
 
@@ -87,3 +90,78 @@ function themNV(){
     // B4: Hiển thị table
     hienThiTable(dsnv.mangNV);
 }
+
+/**
+ * xóa nhân viên
+ * 
+ */
+
+
+function xoaNV(tk){
+    dsnv.xoaNhanVien(tk);
+
+    hienThiTable(dsnv.mangNV);
+    setLocalStorage();
+}
+
+/**
+ * cập nhật nhân viên
+ * 
+ */
+
+
+
+function xemChiTiet(tk){
+    var viTri = dsnv.timViTri(tk);//tìm ra vị trí của nv
+    var nv = dsnv.mangNV[viTri];//truy xuất vị trí của nv đó trong mảng => ta tìm được ptu mangNV[viTri] và gắn cho biến nv
+    getELE("tknv").disabled = true;
+
+    getELE("tknv").value = nv.taiKhoan;//truy xuất thuộc tính taiKhoan của lớp nv 
+    getELE("name").value = nv.hoTenNV;
+    getELE("email").value = nv.email;
+    getELE("password").value = nv.matKhau;
+    getELE("datepicker").value = nv.ngayLam;
+    getELE("luongCB").value = nv.luongCoBan;
+    getELE("chucvu").value = nv.chucVu;
+    getELE("gioLam").value = nv.gioLam;
+}
+
+function capNhatNV(){
+    //B1: lấy thông tin từ form
+    var tk = getELE("tknv").value;
+    var hoTen = getELE("name").value;
+    var email = getELE("email").value;
+    var pass = getELE("password").value;
+    var ngaylam = getELE("datepicker").value;
+    var luong = getELE("luongCB").value;
+    var chucVu = getELE("chucvu").value;
+    var gioLam = getELE("gioLam").value;
+    console.log(tk,hoTen,email,pass,ngaylam,luong,chucVu,gioLam);
+
+    //B2: lưu thông tin vào lớp nhân viên
+    var nv = new NhanVien(tk,hoTen,email,pass,ngaylam,luong,chucVu,gioLam);
+    nv.tongLuong = nv.tinhTongLuong();
+    nv.loaiNV = nv.xepLoaiNV();
+    
+    
+    dsnv.capNhatNhanVien(nv);
+    hienThiTable(dsnv.mangNV);
+    setLocalStorage();
+}
+
+/**
+ * Reset
+ */
+ function resetForm() {
+    getELE("formQLNV").reset();
+    getELE("tknv").disabled = false;
+}
+
+
+
+
+
+
+
+
+
